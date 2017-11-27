2f374d37e0716a7db4c7e3151f8d9b6fee24f55c
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-27 02:07:42 -0600

    <code> Basic Structure for login / sign up

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
 import CurrentUser from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if(loading) { return <div />; }
+    
+    if (user) {
+      return <div>Logout</div>;
+    }else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+      </div>);
+    } 
+  }
+  
   render() {
-    console.log(this.props.data);
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>  
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
