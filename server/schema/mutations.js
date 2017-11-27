a8030ce9f38efa2d1c12a4c02bff0192362a3396
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-27 00:48:27 -0600

    <code> Add User Login / Logout Mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -19,6 +19,22 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, {email, password }, req) {
         return AuthService.signup({ email, password, req });
       }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return AuthService.logout({args},req);
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req});
+      }
     }
   }
 });
